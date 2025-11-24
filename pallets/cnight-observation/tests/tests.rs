594379bf1025b107bfbf698e3fb47d9057c9b1dd
NachoPal <ignacio.palacios.santos@gmail.com>
2025-11-24 18:32:13 +0100

    ci: ignore flaky tests

--- pallets/cnight-observation/tests/tests.rs
+++ pallets/cnight-observation/tests/tests.rs
@@ -483,6 +483,7 @@ fn process_tokens_inherent_should_update_storage_correctly() {
 }
 
 #[test]
+#[ignore = "Falky test - Ignored until fixed"]
 fn removing_duplicate_registration_results_in_valid_registration() {
 	new_test_ext().execute_with(|| {
 		init_ledger_state();
@@ -716,6 +717,7 @@ fn emits_registration_and_mapping_added_on_first_valid_registration() {
 }
 
 #[test]
+#[ignore = "Falky test - Ignored until fixed"]
 fn emits_deregistration_and_mapping_removed_on_last_mapping_removed() {
 	new_test_ext().execute_with(|| {
 		let (cardano_reward_address, dust_public_key) = test_wallet_pairing();
